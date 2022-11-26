bca7da773d08d1f3888f9d555ce9e76c605a618f
Mandy_Li <c26931230@gmail.com>
2022-11-26 13:24:24 +0800

    init

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/cgd103_g2_backend/'
+    : '/'
 })
