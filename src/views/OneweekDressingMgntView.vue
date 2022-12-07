6eb6cb84ee021c3a4825b74a6cd857a187c10111
ling1009 <a3614101aaaaaaa@gmail.com>
2022-12-07 11:15:24 +0800

    12.7"一週穿搭" 管理介面新增

--- src/views/OneweekDressingMgntView.vue
+++ src/views/OneweekDressingMgntView.vue
@@ -1,9 +1,82 @@
 <template>
-<!-- 在這邊寫 -->
-一週穿搭管理Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorum consequatur molestias numquam illum, rem iure! Placeat omnis alias mollitia culpa beatae eveniet architecto ipsam ipsum. Alias reprehenderit vitae libero similique.
+<div class="col-9 px-3 py-3 border border-primary bg-light">
+	<h2>穿搭提案</h2>
+	<table class="table table-striped border border-success p-2 mb-2">
+		<thead>
+			<tr>
+				<th scope="col">星期</th>
+				<th scope="col" colspan="2">男</th>
+				<th scope="col" colspan="2">女</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th scope="row">Monday</th>
+				<td><img class="box" id="image" src="https://picsum.photos/200/300/?random=10"></td>
+				<td><input type="file" placeholder="穿搭組合商品" id="theFile"></td>
+                <td><img src="https://picsum.photos/200/300/?random=10" /></td>
+				<td><input type="file" placeholder="穿搭組合商品"></td>
+
+			</tr>
+			<tr>
+				<th scope="row">Monday</th>
+				<td><img class="box" id="image" src="https://picsum.photos/200/300/?random=10"></td>
+				<td><input type="file" placeholder="穿搭組合商品" id="theFile"></td>
+                <td><img src="https://picsum.photos/200/300/?random=10" /></td>
+				<td><input type="file" placeholder="穿搭組合商品"></td>
+			</tr>
+			<tr>
+				<th scope="row">Monday</th>
+				<td><img class="box" id="image" src="https://picsum.photos/200/300/?random=10"></td>
+				<td><input type="file" placeholder="穿搭組合商品" id="theFile"></td>
+                <td><img src="https://picsum.photos/200/300/?random=10" /></td>
+				<td><input type="file" placeholder="穿搭組合商品"></td>
+			</tr>
+			<tr>
+				<th scope="row">Monday</th>
+				<td><img class="box" id="image" src="https://picsum.photos/200/300/?random=10"></td>
+				<td><input type="file" placeholder="穿搭組合商品" id="theFile"></td>
+                <td><img src="https://picsum.photos/200/300/?random=10" /></td>
+				<td><input type="file" placeholder="穿搭組合商品"></td>
+			</tr>
+			<tr>
+				<th scope="row">Monday</th>
+				<td><img class="box" id="image" src="https://picsum.photos/200/300/?random=10"></td>
+				<td><input type="file" placeholder="穿搭組合商品" id="theFile"></td>
+                <td><img src="https://picsum.photos/200/300/?random=10" /></td>
+				<td><input type="file" placeholder="穿搭組合商品"></td>
+			</tr>
+			<tr>
+				<th scope="row">Monday</th>
+				<td><img class="box" id="image" src="https://picsum.photos/200/300/?random=10"></td>
+				<td><input type="file" placeholder="穿搭組合商品" id="theFile"></td>
+                <td><img src="https://picsum.photos/200/300/?random=10" /></td>
+				<td><input type="file" placeholder="穿搭組合商品"></td>
+			</tr>
+		</tbody>
+	</table>
+</div>
 </template>
 <script>
-    export default {
-	name: "OneweekDressing",
-};
+//     export default {
+// 	name: "OneweekDressing",
+
+    
+//     methods: {
+//         document.getElementById('theFile').onchange = fileChange(){
+//             let file = document.getElementById('theFile').files[0] 
+//         }
+       
+// };
 </script>
+
+<style lang="scss" scope> 
+    input{
+        width: 80px;
+    }
+    .box{
+        border: 1px solid black;
+        width:200px ;
+        height:300px;
+    }
+</style>
