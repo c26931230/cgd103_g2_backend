152449446ea6b115e2ef2ce18c15882872d56b61
Mandy_Li <c26931230@gmail.com>
2022-12-15 17:46:57 +0800

    安裝 axios

--- phpfile/empList.php
+++ phpfile/empList.php
@@ -1,19 +1,15 @@
 <?php
-	//跨域(正式開發不能這樣)
+	//跨域
 	header('Access-Control-Allow-Origin:*');
 	header("Content-Type:application/json;charset=utf-8");
-try{
-	require_once("../connect_cgd103g2.php");
-	$sql = "select * from emp";
-    $employee = $pdo->prepare($sql);
-    $employee->execute();
-    $empRows = $employee->fetchAll(PDO::FETCH_ASSOC);
 
-    echo json_encode($empRows);
-
-}catch(PDOException $e){
-    $result = ["msg"=>"error"];
-    // echo "error";
-    echo json_encode($result);
+  require_once("../connect_cgd103g2.php");
+  $sql = "select * from emp";
+  $employee = $pdo->query($sql);
+  $empRows = $employee->fetchAll();
+  $data = [];
+  foreach ($empRows as $empRow) {
+      $data[] = $empRow;
   }
+  echo json_encode($data);
 ?>
