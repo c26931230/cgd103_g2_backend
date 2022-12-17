6ae9ce271d3d310c0d0c2fd89c31d1dd7e1a7e1a
Mandy_Li <c26931230@gmail.com>
2022-12-17 19:49:12 +0800

    修改員工管理php路徑

---
+++ phpfile/StaffMgnt/empList.php
@@ -0,0 +1,15 @@
+<?php
+	//跨域
+	header('Access-Control-Allow-Origin:*');
+	header("Content-Type:application/json;charset=utf-8");
+
+  require_once("../../connect_cgd103g2.php");
+  $sql = "select * from emp";
+  $employee = $pdo->query($sql);
+  $empRows = $employee->fetchAll();
+  $data = [];
+  foreach ($empRows as $empRow) {
+      $data[] = $empRow;
+  }
+  echo json_encode($data);
+?>
