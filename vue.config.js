a22e3e63866ef70d37eaed49bcd7d9fab444658e
mandy <c26931230@gmail.com>
2022-12-19 19:35:21 +0800

    gitignore npm i 修改vue config

--- vue.config.js
+++ vue.config.js
@@ -14,7 +14,8 @@ module.exports = defineConfig({
 	devServer: {
 		proxy: {
 			'/api_server': {
-				target: 'http://localhost/cgd103_g2_backend/phpfile',
+				target: 'http://localhost/cgd103_g2_backend/phpfile',//本地
+				// target: 'https://tibamef2e.com/cgd103/g2/back/phpfile',//上線
 				changeOrigin: true,
 				pathRewrite: {
 					'^/api_server': ''
