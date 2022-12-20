d280bddd7b667c0dc058239a16278ecfbc132d03
mandy <c26931230@gmail.com>
2022-12-20 23:56:34 +0800

    營運管理及員工管理修正

--- phpfile/OperationMgmt/get_mem_m.php
+++ phpfile/OperationMgmt/get_mem_m.php
@@ -4,15 +4,15 @@
 	header("Content-Type:application/json;charset=utf-8");
 
   require_once("../../connect_cgd103g2.php");
-  $sql = "select months.month, COUNT(member.mem_id)
+  $sql = "SELECT months.month, COUNT(member.mem_id)
   FROM (
     SELECT 1 as month UNION SELECT 2 as month UNION SELECT 3 as month UNION SELECT 4 as month
     UNION SELECT 5 as month UNION SELECT 6 as month UNION SELECT 7 as month UNION SELECT 8 as month
     UNION SELECT 9 as month UNION SELECT 10 as month UNION SELECT 11 as month UNION SELECT 12 as month
   ) as months
-  left join member ON MONTH(member.mem_date) = months.month AND sex = 1
+  left JOIN member ON MONTH(member.mem_date) = months.month AND sex = 1
   GROUP BY months.month
-  order by months.month
+  ORDER BY months.month
   ;";
   // $sql = "select * from emp";
   $member = $pdo->query($sql);
--- phpfile/OperationMgmt/get_mem_sum.php
+++ phpfile/OperationMgmt/get_mem_sum.php
@@ -4,12 +4,21 @@
 	header("Content-Type:application/json;charset=utf-8");
 
   require_once("../../connect_cgd103g2.php");
-  $sql = "select month(mem_date), count(*) from member
-  where year(mem_date) = 2022
-  group by month(mem_date)
-  order by mem_date
+  // $sql = "select month(mem_date), count(*) from member
+  // where year(mem_date) = 2022
+  // group by month(mem_date)
+  // order by mem_date
+  // ;";
+  $sql = "SELECT months.month, COUNT(member.mem_id)
+  FROM (
+    SELECT 1 as month UNION SELECT 2 as month UNION SELECT 3 as month UNION SELECT 4 as month
+    UNION SELECT 5 as month UNION SELECT 6 as month UNION SELECT 7 as month UNION SELECT 8 as month
+    UNION SELECT 9 as month UNION SELECT 10 as month UNION SELECT 11 as month UNION SELECT 12 as month
+  ) as months
+  left JOIN member ON MONTH(member.mem_date) = months.month
+  GROUP BY months.month
+  ORDER BY months.month
   ;";
-  // $sql = "select * from emp";
   $member = $pdo->query($sql);
   $memRows = $member->fetchAll();
   $data = [];
