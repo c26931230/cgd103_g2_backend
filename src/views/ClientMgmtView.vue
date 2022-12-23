be5dc0a0bea460ff1ddce7594c6a30ae44b6f007
Wei <114372757+Sid0478@users.noreply.github.com>
2022-12-23 13:50:45 +0800

    廠商留言頁，可以把tinyint數值轉變成文字

--- src/views/ClientMgmtView.vue
+++ src/views/ClientMgmtView.vue
@@ -21,18 +21,21 @@
       </thead>
       <tbody>
         <tr v-for="e in client" class="item" :key="e.client_id">
-          <th scope="row">{{ e.clientmes_time }}</th>
-          <!-- 留言時間 -->
-          <td>{{ e.client_name }}</td>
-          <!-- 廠商名稱 -->
-          <td>0{{ e.client_phone }}</td>
-          <!-- 廠商名稱 -->
-          <td>{{ e.client_person }}</td>
-          <!-- 留言者姓名 -->
-          <td class="msg">{{ e.client_meg }}</td>
-          <!-- 留言內容 -->
-          <td>{{ e.reply }}</td>
-          <!-- 回覆狀態-->
+            <!-- 留言時間 -->
+            <th scope="row">{{ e.clientmes_time }}</th>
+            <!-- 廠商名稱 -->
+            <td>{{ e.client_name }}</td>
+            <!-- 廠商名稱 -->
+            <td>0{{ e.client_phone }}</td>
+            <!-- 留言者姓名 -->
+            <td>{{ e.client_person }}</td>
+            <!-- 留言內容 -->
+            <td class="msg">{{ e.client_meg }}</td>
+            <!-- 回覆狀態-->
+            <td>
+                <span v-if="e.reply == 0">未回覆</span>
+                <span v-else>已回覆</span>
+            </td>
         </tr>
       </tbody>
     </table>
