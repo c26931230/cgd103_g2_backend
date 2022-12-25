9a1997434176e11c5cd356e0c2ce38a733145c6e
Mandy_Li <c26931230@gmail.com>
2022-12-25 23:48:21 +0800

    營運管理修正

--- phpfile/OperationMgmt/get_mem_sum.php
+++ phpfile/OperationMgmt/get_mem_sum.php
@@ -4,17 +4,13 @@
 	header("Content-Type:application/json;charset=utf-8");
 
   require_once("../../connect_cgd103g2.php");
-  // $sql = "select month(mem_date), count(*) from member
-  // where year(mem_date) = 2022
-  // group by month(mem_date)
-  // order by mem_date
-  // ;";
   $sql = "SELECT months.month, COUNT(member.mem_id)
   FROM (
    SELECT 7 as month UNION SELECT 8 as month
     UNION SELECT 9 as month UNION SELECT 10 as month UNION SELECT 11 as month UNION SELECT 12 as month
   ) as months
   left JOIN member ON MONTH(member.mem_date) = months.month
+  where YEAR(member.mem_date) = 2022
   GROUP BY months.month
   ORDER BY months.month
   ;";
@@ -22,7 +18,6 @@
   $memRows = $member->fetchAll();
   $data = [];
   foreach ($memRows as $memRow) {
-      // $data[] = $memRow[0]; //月
       $data[] = $memRow[1]; //人數
   }
   echo json_encode($data);
